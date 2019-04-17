e19be4b81b6e855860a916eaec183d257185d795
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2019-04-17 15:31:21 -0300

    Tuples

---
+++ Src/FSharp/ConsoleApp/Scripts/TupleStuff.fs
@@ -0,0 +1,15 @@
+﻿module TupleStuff
+
+let tupleReturn() =
+    let avg (w,x,y,z) : float =
+        let sum = w + x + y + z
+        sum / 4.0
+
+    printfn "Avg: %f" (avg(1.0, 2.0, 3.0, 4.0))
+    printfn ""
+
+    let my_data = ("Victor", 23, 6.25)
+
+    let (name,age,_) = my_data
+
+    printfn "Name: %s" name
